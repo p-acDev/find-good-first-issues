be5e9ddaf68e25c4b508f96a4a5fe80ef13c0a40
pacourbet <pierreandre.courbet@gmail.com>
2022-08-25 18:00:45 +0200

    Update find_good.py

--- find_good.py
+++ find_good.py
@@ -1,32 +1,56 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
-topic = "python"
+STATUS = "TEST"
 
-url = f"https://github.com/topics/{topic}"
+try:
+    os.mkdir("./_html_data")
+except FileExistsError:
+    pass
 
-status = "TEST"
+def get_html_data(url, status="TEST"):
 
-def get_html_data():
-
-    # save the output to avoid doing too many queries on the github site during tests =D
-    try:
-        with open(f"_html_data_{url.split('/')[-1]}.txt", 'rb') as f:
-            html_raw_data = f.read()
-            print("[+] File read from already downloaded html")
-    except FileNotFoundError:
+    if STATUS == "TEST":
+    
+        # save the output to avoid doing too many queries on the github site during tests =D
+        try:
+            with open(f"./_html_data/_html_data_{'_'.join(url.split('/')[-2:])}.txt", 'rb') as f:
+                html_raw_data = f.read()
+                print("[+] File read from already downloaded html")
+        except FileNotFoundError:
+            req = requests.get(url)
+            html_raw_data = req.content
+            with open(f"./_html_data/_html_data_{'_'.join(url.split('/')[-2:])}.txt", 'wb') as f:
+                # write in case of further use
+                f.write(html_raw_data)
+            print("[*] File was not available and was downloaded")
+    
+    elif STATUS == "PROD":
+        
+        # to ensure we have up to date data
         req = requests.get(url)
         html_raw_data = req.content
-        with open(f"_html_data_{url.split('/')[-1]}.txt", 'wb') as f:
-            # write in case of further use
-            f.write(html_raw_data)
-        print("[*] File was not available and was downloaded")
         
     return html_raw_data
-    
-soup = BeautifulSoup(get_html_data(), 'html.parser')
 
-# get the list of all repositories
-repo_list = [elem['href'] for elem in soup.find_all("a", class_="text-bold wb-break-word")]
+if __name__ == "__main__":
+    topic = "python"
 
-# find in each repo if there is a good-firt issue
+    url = f"https://github.com/topics/{topic}"
+
+    soup = BeautifulSoup(get_html_data(url), 'html.parser')
+
+    # get the list of all repositories
+    repo_list = [elem['href'] for elem in soup.find_all("a", class_="text-bold wb-break-word")]
+
+    # find in each repo check if there is a good-firt issue
+    good_first_issues = {}
+    for repo in repo_list:
+        # create the new url
+        url = f"https://github.com{repo}/contribute"
+        soup = BeautifulSoup(get_html_data(url), 'html.parser')
+        # find if there is good firt issue
+        __good_first_issues = [elem['href'] for elem in soup.find_all("a", class_="Link--primary h4 js-navigation-open no-underline v-align-middle")]
+        good_first_issues[repo] = __good_first_issues
+        
